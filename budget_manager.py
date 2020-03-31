c9eb436a983b5faa02cb47abe48a29fadd4fee99
Dan Robson <danr.236@gmail.com>
2020-03-31 19:27:50 -0400

    history, month budget, remove month working

--- budget_manager.py
+++ budget_manager.py
@@ -89,16 +89,31 @@ def remove_category(removed_category, date):
 
 
 def remove_month(date):
-    pass
+
+    try:
+        budget[str(date.year)].pop(str(date.month))
+
+        # Remove the year if it's empty
+        if not budget[str(date.year)]:
+            budget.pop(str(date.year))
+        with open(budget_file_loc, "w") as f:
+            f.write(
+                json.dumps(budget, sort_keys=True, indent=4, separators=(",", ": "))
+            )
+    except:
+        print("Month-year pair budget does not exist in " + budget_file_loc)
 
 
-def view_month_budget(month):
-    pass
+def view_month_budget(date):
+    try:
+        print(budget[str(date.year)][str(date.month)]["categories"])
+    except:
+        print("Month-year pair budget does not exist in " + budget_file_loc)
 
 
 # Questionable
-def view_budget_histories():
-    pass
+def view_budget_history():
+    print(budget)
 
 
 def month_budget_report(month):
@@ -127,6 +142,10 @@ if __name__ == "__main__":
         "-b", "--budget", type=float, help="adjusted budget value",
     )
 
+    # To add a category type
+    PARSER.add_argument(
+        "-hist", "--history", dest="history", action="store_true", help="Use",
+    )
     # To remove a category type
     PARSER.add_argument(
         "-r",
@@ -141,16 +160,23 @@ if __name__ == "__main__":
         "--date",
         default=date.today().replace(day=1),
         type=lambda d: datetime.strptime(d, "%Y%m").date(),
-        help="Date of purchase in the format yyyymm. If no date is provided, then the default is today's month.",
+        help="Format yyyymm. Date for input budget. If no date is provided, then the default is today's month.",
     )
 
     # date to apply adjusted budget settings
     PARSER.add_argument(
         "-rmon",
         "--removemonth",
-        default=date.today().replace(day=1),
         type=lambda d: datetime.strptime(d, "%Y%m").date(),
-        help="Date of purchase in the format yyyymm. If no date is provided, then the default is today's month.",
+        help="Format yyyymm. Date for input budget.",
+    )
+
+    # date to apply adjusted budget settings
+    PARSER.add_argument(
+        "-pmon",
+        "--printmonth",
+        type=lambda d: datetime.strptime(d, "%Y%m").date(),
+        help="Format yyyymm. Date for input budget.",
     )
 
     args = PARSER.parse_args()
@@ -160,5 +186,15 @@ if __name__ == "__main__":
 
     if args.add:
         add_category(args.add, args.budget, args.date)
+
     elif args.remove:
         remove_category(args.remove, args.date)
+
+    if args.removemonth is not None:
+        remove_month(args.removemonth)
+
+    if args.printmonth is not None:
+        view_month_budget(args.printmonth)
+
+    if args.history:
+        view_budget_history()
